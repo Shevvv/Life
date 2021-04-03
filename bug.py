9ef11f9b3465cbc11215e1bcb4de224f0e36878f
Shevvv <34458530+Shevvv@users.noreply.github.com>
2021-04-03 08:52:35 +0300

    Add files via upload

--- bug.py
+++ bug.py
@@ -29,7 +29,7 @@ class Bug(Sprite):
         self.rect.y = int(self.y)
 
     def draw_bug(self):
-        #self.update_biomes()
+        self.update_biomes()
 
         pg.draw.circle(self.screen,
                        self.color,
@@ -67,6 +67,7 @@ class Bug(Sprite):
                 self.screen.fill(biome.color, biome.rect)
                 for tree in biome.trees:
                     tree.draw()
+                # pass
 
 
 
